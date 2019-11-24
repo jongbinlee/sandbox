778601619f187a2c6478e9a24439faa3530b76e7
jongbinlee <37610007+jongbinlee@users.noreply.github.com>
2019-11-25 00:03:16 +0900

    test2

--- take1.py
+++ take1.py
@@ -1,3 +1,10 @@
 import pandas as pd
 
-print('hello world!')
+# get iris data set
+df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+
+df.head()
+df.describe()
+
+df.groupby('species')['sepal_width'].mean()
+print(df.groupby('species')['sepal_width'].nunique())
